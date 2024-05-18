056fc6132bc23028d2c7651aa912e5736947e885
alejandromtz04 <not4lmas@gmail.com>
2024-05-18 14:14:24 -0600

    User services
    Adding updateUser and deleteUser

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -6,6 +6,7 @@ import { User } from './entities/user.entity';
 import { CustomHttpException } from 'src/global/custom-exception';
 import { InjectRepository } from '@nestjs/typeorm';
 import * as bcrypt from 'bcryptjs';
+import { find } from 'rxjs';
 @Injectable()
 export class UserService {
 
@@ -118,16 +119,75 @@ export class UserService {
   async updateUser(id: number, updateUserDto: UpdateUserDto) {
     try {
 
-      const findUser = await this.userRepository.findOne({ where: { id, isActivated: true }})
+      const user = await this.userRepository.findOne({ where: { id, isActivated: true }});
+
+      if (!user || user.isActivated == false) {
+        return {
+          ok: false,
+          mesage: "User not found",
+          httpStatus: HttpStatus.NOT_FOUND
+        }
+      }
+
+      const emptyFields = [];
+      Object.entries(updateUserDto).forEach(([key, value]) => {
+        if (value === undefined || value === null || value === ''){
+          emptyFields.push(key)
+        }
+      })
+
+      if (emptyFields.length > 0) {
+        return new BadRequestException(`The following fields are empty or undefined: ${emptyFields.join(', ')}`);
+      }
+
+      user.name = updateUserDto.name;
+      user.lastName = updateUserDto.lastName;
+      user.age = updateUserDto.age;
+      user.userName = updateUserDto.userName;
+      user.password = updateUserDto.password;
+
+      await this.userRepository.save(user);
+      return {
+        ok: true,
+        message: "User saved successfully",
+        status: HttpStatus.OK,
+        object: user
+      }
+      
     } catch (e) {
       throw new CustomHttpException(
-        "Internal server error on method: updateUser in service method \nLines 118-",
-      HttpStatus.INTERNAL_SERVER_ERROR
+        "Internal server error on method: updateUser in service method \nLines 118 - 162",
+        HttpStatus.INTERNAL_SERVER_ERROR
       );
     }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async deleteUser(id: number) {
+    try {
+      const findUser = await this.userRepository.findOne({ where: { id, isActivated: true }})
+
+      if (!findUser || findUser.isActivated == false) {
+        return {
+          ok: false,
+          message: "User not found",
+          HttpStatus: HttpStatus.NOT_FOUND
+        }
+      }
+
+      findUser.isActivated = false;
+
+      await this.userRepository.save(findUser)
+      return {
+        ok: true,
+        message: `User ${findUser.name} was deleted`,
+        status: HttpStatus.OK
+      }
+
+    } catch (e) {
+      throw new CustomHttpException(
+        "Internal server error on method: deleteUser in service method",
+        HttpStatus.INTERNAL_SERVER_ERROR
+      );
+    }
   }
 }
